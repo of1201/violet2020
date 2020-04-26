b133c08293620dfb0ec0b999f398ebad37a2b7bb
of1201 <63320725+of1201@users.noreply.github.com>
2020-04-26 14:18:07 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+//$ is like a java symbol
+$(document).ready(function(){   // upon running the HTML file, the function is called
+
+	function weather(){
+	    var URl = "https://fcc-weather-api.glitch.me/api/current?lat=53.70&lon=-1.24"; //API address
+	
+	    $.getJSON(URL, function(data){
+		console.log(data);    //after entering into the URL, we assign all the data into the data variable. And use console.log to run all the data in the browser
+	    });
+	}  //function end does not need a semicolon
+
+	weather();
+});
