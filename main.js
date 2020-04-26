36577ca8058540467b6bc14874b3b5c7cae45ebc
of1201 <63320725+of1201@users.noreply.github.com>
2020-04-26 14:45:24 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 $(document).ready(function(){   // upon running the HTML file, the function is called
 
 	function weather(){
-	    var URl = "https://fcc-weather-api.glitch.me/api/current?lat=53.70&lon=-1.24"; //API address
+	    var URL = 'https://fcc-weather-api.glitch.me/api/current?lat=53.70&lon=-1.24'; //API address
 	
 	    $.getJSON(URL, function(data){
 		console.log(data);    //after entering into the URL, we assign all the data into the data variable. And use console.log to run all the data in the browser
